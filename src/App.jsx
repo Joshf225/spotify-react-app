aa8f81e0fad8b3242e0716e666a08fecc8e5a5c5
JOSHUA FAMOSE <joshua.famose.2021@mumail.ie>
2023-11-29 11:39:53 +0000

    added remove track button

--- src/App.jsx
+++ src/App.jsx
@@ -171,6 +171,15 @@ function App() {
   };
 
   const renderFilteredTracks = () => {
+    const removeTrack = (trackIdToRemove) => {
+      // Use filter to create a new array without the removed track
+      const updatedTracks = filteredTracks.filter(
+        ({ track }) => track.id !== trackIdToRemove
+      );
+
+      // Set the new array of tracks (assuming 'filteredTracks' is a state variable)
+      setFilteredTracks(updatedTracks); // You may need to use state management here
+    };
     return filteredTracks.map(({ track }) => (
       <div
         key={track.id}
@@ -199,45 +208,61 @@ function App() {
           <h2 style={{ color: "white", textOverflow: "hidden" }}>
             {track.name}
           </h2>
+          <button
+            onClick={() => removeTrack(track.id)}
+            style={{
+              backgroundColor: "#FF0000",
+              color: "#FFFFFF",
+              padding: "0.5rem 1rem",
+              borderRadius: "0.5rem",
+              border: "none",
+              cursor: "pointer",
+              marginTop: "1rem",
+            }}
+          >
+            Remove Track
+          </button>
           {track?.preview_url && (
-            <a
-              className="preview-btn"
-              target="_blank"
-              href={track?.preview_url}
-              style={{
-                textDecoration: "none",
-                backgroundColor: "#427D9D",
-              }}
-            >
-              <span className="preview-btn__icon-wrapper">
-                <svg
-                  width="10"
-                  className="preview-btn__icon-svg"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 15"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
-                  ></path>
-                </svg>
+            <>
+              <a
+                className="preview-btn"
+                target="_blank"
+                href={track?.preview_url}
+                style={{
+                  textDecoration: "none",
+                  backgroundColor: "#427D9D",
+                }}
+              >
+                <span className="preview-btn__icon-wrapper">
+                  <svg
+                    width="10"
+                    className="preview-btn__icon-svg"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 15"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                    ></path>
+                  </svg>
 
-                <svg
-                  className="preview-btn__icon-svg  preview-btn__icon-svg--copy"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="10"
-                  fill="none"
-                  viewBox="0 0 14 15"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
-                  ></path>
-                </svg>
-              </span>
-              Preview Sound
-            </a>
+                  <svg
+                    className="preview-btn__icon-svg  preview-btn__icon-svg--copy"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    fill="none"
+                    viewBox="0 0 14 15"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                    ></path>
+                  </svg>
+                </span>
+                Preview Sound
+              </a>
+            </>
           )}
         </div>
       </div>
